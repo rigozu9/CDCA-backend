7eff8e47c85ac53ea0e6e7d122f2b67f91a6b748
Riku <134082872+rigozu9@users.noreply.github.com>
2023-10-26 00:09:59 +0300

    frontend added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const unknownEndpoint = (request, response) => {
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 
